fa40f0260c3b31db24a3920c84e07fc92494c134
steven <stvncruickshank@gmail.com>
2018-01-21 16:34:16 -0500

    just cleaning up some code!

--- SCruickshankFinalProject/AboutDlg.h
+++ SCruickshankFinalProject/AboutDlg.h
@@ -7,17 +7,17 @@ class AboutDlg : public CDialogEx
 {
 	DECLARE_DYNAMIC(AboutDlg)
 
-public:
-	AboutDlg(CWnd* pParent = NULL);   // standard constructor
-	virtual ~AboutDlg();
+	public:
+		AboutDlg(CWnd* pParent = NULL);   // standard constructor
+		virtual ~AboutDlg();
 
-// Dialog Data
-#ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ABOUTDLG };
-#endif
+	// Dialog Data
+	#ifdef AFX_DESIGN_TIME
+		enum { IDD = IDD_ABOUTDLG };
+	#endif
 
-protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	protected:
+		virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
 };
--- SCruickshankFinalProject/AttribDlg.h
+++ SCruickshankFinalProject/AttribDlg.h
@@ -8,48 +8,54 @@ class AttribDlg : public CDialogEx
 {
 	DECLARE_DYNAMIC(AttribDlg)
 
-public:
-	AttribDlg(CWnd* pParent = NULL);   // standard constructor
-	virtual ~AttribDlg();
+	public:
+		AttribDlg(CWnd* pParent = NULL);   // standard constructor
+		virtual ~AttribDlg();
 
 // Dialog Data
-#ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_ATTRIB };
-#endif
+	#ifdef AFX_DESIGN_TIME
+		enum { IDD = IDD_ATTRIB };
+	#endif
 
-protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	protected:
+		virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
-public:
-	CButton m_rArchive;
-	CButton m_rCompress;
-	CButton m_rEncrypt;
-	CButton m_rHidden;
-	CButton m_rNormal;
-	CButton m_rRdOnly;
-	CButton m_bOK;
-	CButton m_bCancel;
-	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedCancel();
-	afx_msg void OnEnChangeEdit3();
-	CEdit m_ePath;
-	CString tempA;
-	CString tempB;
-	int archive;
-	int compress;
-	int encrypt;
-	int hidden;
-	int normal;
-	int readonly;
-	int indexed;
-	int system;
-	int offline;
-	int temp;
-	int vrt;
-	CButton m_rIndexed;
-	CButton m_rSystem;
-	CButton m_rOffline;
-	CButton m_rTemp;
-	CButton m_rVirtual;
+
+	//member variables include widgets, and ints
+	public:
+		CButton m_rArchive;
+		CButton m_rCompress;
+		CButton m_rEncrypt;
+		CButton m_rHidden;
+		CButton m_rNormal;
+		CButton m_rRdOnly;
+		CButton m_bOK;
+		CButton m_bCancel;
+		CButton m_rIndexed;
+		CButton m_rSystem;
+		CButton m_rOffline;
+		CButton m_rTemp;
+		CButton m_rVirtual;
+		CEdit m_ePath;
+		CString tempA;
+		CString tempB;
+		
+		int archive;
+		int compress;
+		int encrypt;
+		int hidden;
+		int normal;
+		int readonly;
+		int indexed;
+		int system;
+		int offline;
+		int temp;
+		int vrt;
+
+		//event handlers
+		afx_msg void OnBnClickedOk();
+		afx_msg void OnBnClickedCancel();
+		afx_msg void OnEnChangeEdit3();
+		
 };
